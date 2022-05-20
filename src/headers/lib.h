0abc94f6d238aed388d37d6e06472579c1d7d98d
NanReven <nanreven@gmail.com>
2022-05-20 19:55:22 +0300

    first commit

--- src/headers/lib.h
+++ src/headers/lib.h
@@ -3,13 +3,42 @@
 
 #ifdef __PL_WINDOWS__
 
-// Begin exception handling
-#define BEGIN_EXCEPTION_HANDLING \
-	; // TODO: Михаил :)
+#include <setjmp.h>
+#include <signal.h>
+#include <stdlib.h>
 
-// End exception handling
-#define END_EXCEPTION_HANDLING \
-	; // TODO: Михаил :)
+jmp_buf savebuf;
+
+void (*fpe_signal_react)(int);
+void (*segv_signal_react)(int);
+void (*abrt_signal_react)(int);
+
+void fpe_signal_handler(int sig);
+void segv_signal_handler(int sig);
+void abrt_signal_handler(int sig);
+
+enum Errors {
+	ZeroDivisionError = 1,
+	SegmentationFault,
+	Aborted
+};
+
+char* file;  
+int line;  
+int jmp; 
+
+#define BEGIN_EXCEPTION_HANDLING \
+	jmp = setjmp(savebuf); \
+	if (jmp == 0) { \
+		file = __FILE__; \
+		line = __LINE__; \
+		fpe_signal_react = signal(SIGFPE, fpe_signal_handler); \
+		segv_signal_react = signal(SIGSEGV, segv_signal_handler); \
+		abrt_signal_react = signal(SIGABRT, abrt_signal_handler); \
+	} \
+	if (jmp == 0)
+
+#define END_EXCEPTION_HANDLING else {}
 
 #endif // __PL_WINDOWS__
 
@@ -18,8 +47,6 @@
 #include <setjmp.h>
 #include <signal.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 
 jmp_buf savebuf;
 
@@ -33,9 +60,9 @@ enum Errors {
 	Aborted
 };
 
-char* file;
-int line;
-int jmp;
+char* file;  
+int line;  
+int jmp; 
 
 // Begin exception handling
 #define BEGIN_EXCEPTION_HANDLING \
--- src/sources/exception.c
+++ src/sources/exception.c
@@ -3,7 +3,29 @@
 
 #ifdef __PL_WINDOWS__
 
-// TODO: Михаил :)
+void segv_signal_handler(int sig) {
+	printf("%s:%d: exception: Segmentation fault (error code %d)\n", file, line, SegmentationFault);
+	if (signal(SIGSEGV, segv_signal_react) == SIG_ERR) {
+		perror("Error restoring a default signal");
+	}
+	longjmp(savebuf, SegmentationFault);
+}
+
+void fpe_signal_handler(int sig) {
+	printf("%s:%d: exception: Division by zero (error code %d)\n", file, line, ZeroDivisionError);
+	if (signal(SIGFPE, fpe_signal_react) == SIG_ERR) {
+		perror("Error restoring a default signal");
+	}
+	longjmp(savebuf, ZeroDivisionError);
+}
+
+void abrt_signal_handler(int sig) {
+	printf("%s:%d: exception: Aborted (error code %d)\n", file, line, Aborted);
+	if (signal(SIGABRT, abrt_signal_react) == SIG_ERR) {
+		perror("Error restoring a default signal");
+	}
+	longjmp(savebuf, Aborted);
+}
 
 #endif // __PL_WINDOWS__
 
